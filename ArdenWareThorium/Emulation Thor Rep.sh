0617bcb2d4c48f4000a3b2248dacfdc42448ee76
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-25 15:51:03 -0500

    Create Emulation Thor Rep.sh

---
+++ ArdenWareThorium/Emulation Thor Rep.sh
@@ -0,0 +1,13 @@
+
+echo  ____  ______ |                   |______  "
+echo "|___     |   |_______  _______   |           "
+echo "|        |   |       |  |      |  |          "
+echo "|___     |   |       |  |      |  |        "
+echo "                     |  |______|              "
+echo  -------------------------------------------------------
+echo  Welcome to the Emulation Thor Rep. This Rep contains a version of Emulation Thor that is
+echo  customly modified to be used in this Rep for easy install. Contains lots of ArdenWareApps.
+dialog --menu "Choose one:" 10 30 3 \
+    1 Red \
+    2 Green \
+    3 ArdenWARE
