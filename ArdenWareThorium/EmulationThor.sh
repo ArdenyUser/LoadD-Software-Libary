b8317599069927ae8d193ad58f60bdab09e8d57c
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-25 08:38:45 -0500

    Update EmulationThor.sh

--- ArdenWareThorium/EmulationThor.sh
+++ ArdenWareThorium/EmulationThor.sh
@@ -1,16 +1,22 @@
 # license - by Avery Stafford 2021-2022
 echo EMULATION-THOR
 echo Put in any directory with code.ethor file -ETHOR is a zip with Thor/ArdenWare code in it- and activate EMULATION-THOR.
-echo Begin? Y -CAPS- or N:
+echo Begin? Y -CAPS- or N :
 read rqa
-echo What dir will the ETHOR be?
+if [ $rqa -eq Y ] then
+echo What dir will the ETHOR be? :
 read location
 cd $location
-if [ $rqa -eq Y ] then
-mv ./code.ethor ./code.zip
-unzip code.zip
+echo What is the name of the .ethor file? -do not include the extension ETHOR :
+read fdata
+mv ./$fdata.ethor ./$fdata.zip
+unzip $fdata.zip
 wget https://github.com/ArdenyUser/LoadD-Software-Libary/raw/main/ArdenWareThorium/emulator.zip
 unzip emulator.zip
 # ADD AARCH64 MAKEFILE OR ARM MAKEFILE
-make
+echo Which installation which you have? arm or aarch64
+read archti
+arm-linux-gnueabi-gcc main.c -o helloworld-arm -static
+if [ $archti == "arm" ]; arm-linux-gnueabi-gcc main.c -o helloworld-arm $fdata-static; fi
+if [ $archti == "aarch64" ]; aarch64-linux-gnueabi-gcc main.c -o $fdata-static; fi
 fi
