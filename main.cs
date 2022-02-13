da45a64818a73cb90daaf92abe42aad1d7acd8d3
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2022-02-13 12:51:19 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,16 +2,19 @@ using System;
 using System.IO;
 using System.Text;
 
-class Demo
+class Main
 {
     static void Main()
     {
         string path = @"c:\temp\SCRIPT.vis";
-        
+
         char previous = '';
         string all = "";
         string cmd = "";
-        
+        string scpy;
+        int mode = 0;
+        string aug2;
+
         Stream s = new FileStream(@"data.vi", FileMode.Open);
         int val = 0;
         char ch;
@@ -24,30 +27,52 @@ class Demo
                 break;
             ch = (char)val;
             Console.Write(ch);
-            if (ch != "(")
+            if (ch != '(')
             {
-               all = Concat(all, ch);
-               
-               
+                all = Concat(all, ch);
+
+
             }
             else
             {
-                cmd = all;
+                mode = 1;
                 all = "";
-                all = Concat(all, ch);
-                
+
             }
-            if (ch == ")")
+            if (mode = 1)
+            {
+                cmd = all;
+                all = Concat(all, ch);
+            }
+            if (ch == ',')
+            {
+                mode = 2;
+                all = "";
+            }
+            if (mode = 2)
+            {
+                aug2 = all;
+                all = Concat(all, ch);
+            }
+            if (ch == ')')
             {
                 if (cmd == "embed")
                 {
                     File.Copy("C:/Visix/Embeds/" + all, all);
                 }
-                if (cmd == "copyFile")
+                if (cmd == "copy")
                 {
-                    File.Copy("C:/Visix/Embeds/" + all, all);
-                }   
-            }    
+                    File.AppendAllText(@"main.vis", "File.Copy(" + all", " + aug2");" + Environment.NewLine);
+                }
+                if (cmd == "using")
+                {
+                    File.AppendAllText(@"main.vis", "using " + all + Environment.NewLine);
+                }
+                if (cmd == "fileAppend")
+                {
+                    File.AppendAllText(@"main.vis", "File.AppendAllText(@" + all + ',' + aug2 " + Environment.NewLine);" + Environment.NewLine);
+                }
+            }
         }
         Console.WriteLine();
     }
